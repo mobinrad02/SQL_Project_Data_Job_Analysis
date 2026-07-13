e0eb1b139261eba46faecf8d844ea2f77a4125c5
Mobin Rad <mobinrad@Parands-MacBook-Pro.local>
2026-07-13 11:09:07 -0600

    Remove save code folder from repository

--- save code/14 SUB QUERY.sql
+++
@@ -1,7 +0,0 @@
---2:49:09
-SELECT *
-FROM (
-    SELECT *
-    FROM job_postings_fact
-    WHERE EXTRACT (MONTH FROM job_posted_date) = 1
-) AS january_jobs;
--- save code/6 SELECT TYPE.sql
+++
@@ -1,6 +0,0 @@
---2:30:16
-SELECT
-    '2023-02-19'::DATE,
-    '123'::INTEGER,
-    'true'::BOOLEAN,
-    '3.14'::REAL;
